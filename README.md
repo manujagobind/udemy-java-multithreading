4d9e83458fc41c5ab8910464d1fbbc8380298e98
manujagobind <manujagobind@gmail.com>
2021-11-24 21:14:37 +0530

    Simple thread creation - part 1, add notes

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 # Learning multi-threading in Java
 
-Learning from (https://www.udemy.com/course/java-multithreading-concurrency-performance-optimization/)[this Udemy course.]
+Learning from [this Udemy course](https://www.udemy.com/course/java-multithreading-concurrency-performance-optimization/).
 
+## Notes
+
+- Need for multi-threading
+  - Responsiveness via concurrency: servers able to server multiple requests at once, moving a mouse on a screen during video streaming displays playback controls
+  - Performance: running tasks in paralell
+
+
+- Single threaded Process vs Multi-threaded Process
+
+  - ![Single Threaded Process](assets/process-single-threaded.png)
+  - ![Multi-threaded Process](assets/process-multi-threaded.png)
---
+++ src/thread/creation/example1/Main.java
@@ -0,0 +1,24 @@
+package thread.creation.example1;
+
+public class Main {
+    
+    public static void main(String args[]) {
+
+        Thread thread = new Thread(new Runnable(){
+
+            @Override
+            public void run() {
+
+                System.out.println("Hello from " + Thread.currentThread().getName());
+                System.out.println("Thread priority is " + Thread.currentThread().getPriority());
+            }
+        });
+
+        thread.setName("my-worker-thread");
+        thread.setPriority(Thread.MAX_PRIORITY);
+
+        System.out.println("Hello from " + Thread.currentThread().getName() + " before starting a new thread.");
+        thread.start();
+        System.out.println("Hello from " + Thread.currentThread().getName() + " after starting a new thread.");
+    }
+}
