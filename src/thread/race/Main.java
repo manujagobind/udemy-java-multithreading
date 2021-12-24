edad1c84cd6f6ca374246d191ceba29a056f7227
manujagobind <manujagobind@gmail.com>
2021-12-24 20:43:06 +0530

    Threads: sequential execution

---
+++ src/thread/race/Main.java
@@ -0,0 +1,72 @@
+package thread.race;
+
+public class Main {
+
+    public static void main (String args[]) throws InterruptedException {
+
+        InventoryCounter inventoryCounter = new InventoryCounter();
+
+        Thread incrementingThread = new IncrementingThread(inventoryCounter);
+        Thread decrementingThread = new DecrementingThread(inventoryCounter);
+
+        
+        // Sequential exection
+        incrementingThread.start();
+        incrementingThread.join();
+
+        decrementingThread.start();
+        decrementingThread.join();
+
+        System.out.println("The count of items is: " + inventoryCounter.getItems());
+    }
+
+    private static class IncrementingThread extends Thread {
+
+        InventoryCounter inventoryCounter;
+
+        public IncrementingThread(InventoryCounter inventoryCounter) {
+            this.inventoryCounter = inventoryCounter;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0 ; i < 1000 ; i++) {
+                this.inventoryCounter.increment();
+            }
+        }
+    }
+
+    private static class DecrementingThread extends Thread {
+
+        InventoryCounter inventoryCounter;
+
+        public DecrementingThread(InventoryCounter inventoryCounter) {
+            this.inventoryCounter = inventoryCounter;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0 ; i < 1000 ; i++) {
+                this.inventoryCounter.decremen();
+            }
+        }
+    }
+
+    private static class InventoryCounter {
+
+        private int items = 0;
+
+        public void increment() {
+            this.items++;
+        }
+
+        public void decremen() {
+            this.items--;
+        }
+
+        public int getItems() {
+            return items;
+        }
+    }
+    
+}
